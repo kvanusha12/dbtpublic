991f6774c9290f932de3ecb34cc71867080f6972
Sunil Vaggannavar <sunilvaggannavar@Sunil-Vaggannavar.local>
2025-05-14 14:31:02 +0200

    Prep1

---
+++ models/prep/prep_weather_daily.sql
@@ -0,0 +1,26 @@
+WITH daily_data AS (
+    SELECT * 
+    FROM {{ref('staging_weather_daily')}}
+),
+add_features AS (
+    SELECT *
+        , DATE_PART('day', date) AS date_day        -- number of the day of month
+        , DATE_PART('month', date) AS date_month    -- number of the month of year
+        , DATE_PART('year', date) AS date_year      -- number of year
+        , DATE_PART('week', date) AS cw             -- number of the week of year
+        , TO_CHAR(date, 'FMMonth') AS month_name    -- name of the month
+        , TRIM(TO_CHAR(date, 'Day')) AS weekday         -- name of the weekday
+    FROM daily_data 
+),
+add_more_features AS (
+    SELECT *
+        , (CASE 
+            WHEN month_name in ('December', 'January', 'February') THEN 'winter'
+            WHEN month_name in ('March', 'April', 'May') THEN 'spring'
+            WHEN month_name in ('June', 'July', 'August') THEN 'summer'
+            WHEN month_name in ('September', 'October', 'November') THEN 'autumn'
+           END) AS season
+    FROM add_features
+)
+SELECT *
+FROM add_more_features
